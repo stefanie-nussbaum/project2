bf02d8daf69636f17fe7ef798347dd69c31aa58b
Stefanie <stefanien001@gmail.com>
2021-07-13 10:06:10 -0500

    components created and routed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
 import './App.css';
+import Navbar from "./components/Navbar"
+import Footer from './components/Footer'
+import Home from "./components/Home"
+import Movies from "./components/Movies"
+import Shows from './components/Shows'
+import AddNew from "./components/AddNew"
+import Details from "./components/Details"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/movies">
+        <Movies />
+      </Route>
+      <Route path="/shows">
+        <Shows />
+      </Route>
+      <Route path="/add-new">
+        <AddNew />
+      </Route>
+      <Route>
+        <Details />
+      </Route>
+      <Footer />
+
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <div>
+      Navbar
+      <h1>Watch Party</h1>
+      <Link>
+        <h4>Movies</h4>
+      </Link>
+    </div>
+  )
+}
