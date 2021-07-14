403be75473c4cbeb73274c6baf686edf78ebcccb
Stefanie <stefanien001@gmail.com>
2021-07-14 09:09:48 -0500

    filter shows section working

--- src/components/Shows.jsx
+++ src/components/Shows.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import { URL, headers } from "../services"
+import MediaCard from './MediaCard'
 
 export default function Shows() {
+  const [shows, setShows] = useState([])
+
+  useEffect(() => {
+    const fetchShows = async () => {
+      const res = await axios.get(URL, { headers })
+      setShows(res.data.records.filter(show => {
+        if (!show.fields.movie) {
+          return show
+        }
+        return null
+      }))
+    }
+    fetchShows()
+  }, [])
+
   return (
     <div>
       Shows
+      {shows.map(show => {
+        return <MediaCard key={show.id} media={show} />
+      })}
     </div>
   )
 }
