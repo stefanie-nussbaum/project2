45eab2ea38358978d62f03bf52a2570918c1e90a
Stefanie <stefanien001@gmail.com>
2021-07-16 10:05:32 -0500

    fixed footer links, added shadows to images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ export default function Footer() {
   return (
     <div className="footer">
       <h3>By Stefanie Nussbaum</h3>
-      <Link to={"https://github.com/stefanie-nussbaum"} >
+      <Link to={{ pathname: "https://github.com/stefanie-nussbaum" }} target="_blank" >
         <img className="icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png" />
       </Link>
-      <Link to={"https://www.linkedin.com/in/stefanie-nussbaum-9a9369135/"}>
+      <Link to={{ pathname: "https://www.linkedin.com/in/stefanie-nussbaum-9a9369135/" }} target="_blank" >
         <img className="icon" src="http://simpleicon.com/wp-content/uploads/linkedin.png" />
       </Link>
     </div>
